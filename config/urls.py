05868ecd295e4126f521d14cf2c31ef695ef28db
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-05-29 22:52:57 +0300

    ✨ Start with models

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,22 @@
 from datetime import datetime, timedelta
 from dataclasses import dataclass, asdict
 import json
+from random import choice, randint
+from string import ascii_letters
 
 import requests
 from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
+from core.models import User
 
 
 def filter_by_keys(source: dict, keys: list[str]) -> dict:
     filtered_data = {}
-
     for key, value in source.items():
         if key in keys:
             filtered_data[key] = value
-
     return filtered_data
 
 
@@ -91,8 +92,53 @@ def get_pokemon_for_mobile(request, name: str):
     )
 
 
+# *****************************************************
+# All roles are hardcoded instead of beeing used in the database
+# *****************************************************
+
+ROLES = {
+    "ADMIN": 1,
+    "MANAGER": 2,
+    "USER": 3,
+}
+
+
+def _get_random_string(size: int = 5) -> str:
+    return "".join([choice(ascii_letters) for _ in range(size)])
+
+
+def create_random_user(request):
+    email_prefix = _get_random_string(size=randint(5, 8))
+    email_affix = _get_random_string(size=randint(2, 5))
+    email = "".join((email_prefix, "@", email_affix, ".com"))
+
+    user = User.objects.create(
+        username=_get_random_string(size=randint(5, 10)),
+        email=email,
+        first_name=_get_random_string(size=randint(5, 10)),
+        last_name=_get_random_string(size=randint(5, 10)),
+        password=_get_random_string(size=randint(10, 20)),
+        role=ROLES["USER"],
+    )
+
+    result = {
+        "id": user.pk,
+        "username": user.username,
+        "email": user.email,
+        "firstName": user.first_name,
+        "lastName": user.last_name,
+        "role": user.role,
+    }
+
+    return HttpResponse(
+        content_type="application/json",
+        content=json.dumps(result),
+    )
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/pokemon/<str:name>/", get_pokemon),
     path("api/pokemon/mobile/<str:name>/", get_pokemon_for_mobile),
+    path("create-random-user", create_random_user),
 ]
