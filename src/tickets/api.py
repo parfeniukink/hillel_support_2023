fc7ad0597930e083492d0873369cd57d972045c0
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-06-26 22:22:56 +0300

    Messages are added

--- src/tickets/api.py
+++ src/tickets/api.py
@@ -1,17 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db.models import Q
-from rest_framework.permissions import BasePermission, IsAuthenticated
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from tickets.models import Ticket
-from tickets.serializers import TicketSerializer, TicketAssignSerializer
+
+from tickets.models import Ticket, Message
+from tickets.permissions import IsOwner, RoleIsAdmin, RoleIsManager, RoleIsUser
+from tickets.serializers import (
+    MessageSerializer,
+    TicketAssignSerializer,
+    TicketSerializer,
+)
 from tickets.services import AssignService
 from users.constants import Role
 
-
-from tickets.permissions import RoleIsAdmin, RoleIsManager, IsOwner, RoleIsUser
+User = get_user_model()
 
 
 class TicketAPIViewSet(ModelViewSet):
@@ -82,8 +90,39 @@ class TicketAPIViewSet(ModelViewSet):
 
 
 class MessageListCreateAPIView(ListCreateAPIView):
-    serializer_class = TicketSerializer
+    serializer_class = MessageSerializer
+    lookup_field = "ticket_id"
 
     def get_queryset(self):
-        # TODO: Start from here
-        raise NotImplementedError
+        # ticket = get_object_or_404(
+        #     Ticket.objects.all(), id=self.kwargs[self.lookup_field]
+        # )
+        # if ticket.user != self.request.user and ticket.manager != self.request.user:
+        #     raise Http404
+
+        return Message.objects.filter(
+            Q(ticket__user=self.request.user) | Q(ticket__manager=self.request.user),
+            ticket_id=self.kwargs[self.lookup_field],
+        )
+
+    @staticmethod
+    def get_ticket(user: User, ticket_id: int) -> Ticket:
+        """Get tickets for current user."""
+
+        tickets = Ticket.objects.filter(Q(user=user) | Q(manager=user))
+        return get_object_or_404(tickets, id=ticket_id)
+
+    def post(self, request, ticket_id: int):
+        ticket = self.get_ticket(request.user, ticket_id)
+        payload = {
+            "text": request.data["text"],
+            "ticket": ticket.id,
+        }
+        serializer = self.get_serializer(data=payload)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
--- src/tickets/urls.py
+++ src/tickets/urls.py
@@ -6,5 +6,5 @@ from tickets.api import TicketAPIViewSet, MessageListCreateAPIView
 router = DefaultRouter()
 router.register("", TicketAPIViewSet, basename="tickets")
 urlpatterns = router.urls + [
-    path("<int:ticket_id>/messages", MessageListCreateAPIView.as_view())
+    path("<int:ticket_id>/messages/", MessageListCreateAPIView.as_view())
 ]
